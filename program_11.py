3e70fe8dd69b9f9ae4bdbb159a368188051c0a6f
Marissa Lynn Cubbage <mcubbage@scholar-fe06.rcac.purdue.edu>
2020-04-19 13:22:50 -0400

    <mcubbage>

--- program_11.py
+++ program_11.py
@@ -76,112 +76,112 @@ if __name__ == '__main__':
 
 
 
-#get daily flow for the past 5 years for both streams
-ReadData('TippecanoeRiver_Discharge_03331500_19431001-20200315.txt')
-DataDF, MissingValues=ClipData( DataDF,'2014-10-01', '2019-09-30') 
-Tippe_5=DataDF
-ReadData('WildcatCreek_Discharge_03335000_19540601-20200315.txt')
-DataDF, MissingValues= ClipData( DataDF,'2014-10-01', '2019-09-30')
-Wildcat_5=DataDF
-
-
-#plot daily flow for both streams for the past 5 years
-plt.subplot(211)
-plt.plot(Tippe_5["Discharge"], 'k^', label='Tippecanoe River')
-plt.legend()
-plt.show()
-plt.ylim(1,15000)
-
-plt.subplot(212)   
-plt.plot(Wildcat_5["Discharge"], 'k+', label='Wildcat River')
-plt.xlabel("Date")
-plt.ylim(1,15000)
-plt.ylabel(".                                Discharge (cubic feet per second)")
-plt.legend()
-
-plt.savefig('daily_csf.png', dpi=96)
-
-
-#read in data  
-Ann_met=ReadMetrics('Annual_Metrics.csv')  
-Month_met=ReadMetrics('Monthly_Metrics.csv')
-tippe_met=Ann_met[Ann_met['Station']=='Tippe']
-wildcat_met=Ann_met[Ann_met['Station']=='Wildcat']
-
-
-#plot annual coeff of var
-plt.plot(tippe_met["Coeff Var"], 'k*', label='Tippecanoe River')
-plt.plot(wildcat_met["Coeff Var"], 'k+', label='Wildcat River')
-plt.xlabel("Year")
-plt.ylabel("Coefficient of Variation")
-#ax=plt.gca()
-#ax.set_xticklabels(np.arange(1969,2019,50))
-plt.tight_layout()
-plt.legend()
-plt.show()
-
-plt.savefig('coeffvar.png', dpi=96)
-
-#plot annual tqmean
-plt.plot(tippe_met["Tqmean"], 'k*', label='Tippecanoe River')
-plt.plot(wildcat_met["Tqmean"], 'k+', label='Wildcat River')
-plt.xlabel("Year")
-plt.ylabel("TQMean (%)")
-#ax=plt.gca()
-#ax.set_xticklabels(np.arange(1969,2019,50))
-plt.tight_layout()
-plt.legend()
-plt.savefig('tqmean.png', dpi=96)
-
-#plot annual RBrichards baker flashiness index  
-
-
-plt.plot(tippe_met["R-B Index"], 'k*', label='Tippecanoe River')
-plt.plot(wildcat_met["R-B Index"], 'k+', label='Wildcat River')
-plt.xlabel("Year")
-plt.ylabel("Richards Baker Flashiness Index")
-#ax=plt.gca()
-#ax.set_xticklabels(np.arange(1969,2019,50))
-plt.tight_layout()
-plt.legend()
-plt.show()
-plt.savefig('rbindex.png', dpi=96)
-
-
-#calculate exceedence probability
-
-ep_tippe=tippe_met.drop(columns=['site_no', 'Mean Flow', 'Median Flow', 'Coeff Var', 'Skew', 'Tqmean', 'R-B Index', '7Q', '3xMedian'])
-tippe_flow=ep_tippe.sort_values('Peak Flow', ascending=False)
-tippe_ranks1=stats.rankdata(tippe_flow['Peak Flow'], method='average')
-tippe_ranks2=tippe_ranks1[::-1]
-tippe_ep=[(tippe_ranks2[i]/(len(tippe_flow)+1)) for i in range(len(tippe_flow))]
-
-
-ep_wild=wildcat_met.drop(columns=['site_no', 'Mean Flow', 'Median Flow', 'Coeff Var', 'Skew', 'Tqmean', 'R-B Index', '7Q', '3xMedian'])
-wild_flow=ep_wild.sort_values('Peak Flow', ascending=False)
-wild_ranks1=stats.rankdata(wild_flow['Peak Flow'], method='average')
-wild_ranks2=wild_ranks1[::-1]
-wild_ep=[(wild_ranks2[i]/(len(wild_flow)+1)) for i in range(len(wild_flow))]
-
-#plot exceeedence probability
-plt.plot(tippe_ep, tippe_flow['Peak Flow'], label='Tippecanoe River', color='black')
-plt.plot(wild_ep, wild_flow['Peak Flow'], label='Wildcat River')
-plt.xlabel("Exceedence Probability")
-plt.ylabel("Peak Discharge (CFS)")
-plt.tight_layout()
-plt.legend()
-plt.show()
-
-
-plt.savefig('exceedence.png', dpi=96)
-
+    #get daily flow for the past 5 years for both streams
+    ReadData('TippecanoeRiver_Discharge_03331500_19431001-20200315.txt')
+    DataDF, MissingValues=ClipData( DataDF,'2014-10-01', '2019-09-30') 
+    Tippe_5=DataDF
+    ReadData('WildcatCreek_Discharge_03335000_19540601-20200315.txt')
+    DataDF, MissingValues= ClipData( DataDF,'2014-10-01', '2019-09-30')
+    Wildcat_5=DataDF
+    
+    
+    #plot daily flow for both streams for the past 5 years
+    plt.subplot(211)
+    plt.plot(Tippe_5["Discharge"], 'k^', label='Tippecanoe River')
+    plt.legend()
+    plt.show()
+    plt.ylim(1,15000)
+    
+    plt.subplot(212)   
+    plt.plot(Wildcat_5["Discharge"], 'k+', label='Wildcat River')
+    plt.xlabel("Date")
+    plt.ylim(1,15000)
+    plt.ylabel(".                                Discharge (cubic feet per second)")
+    plt.legend()
+    
+    plt.savefig('daily_csf.png', dpi=96)
+    
+    
+    #read in data  
+    Ann_met=ReadMetrics('Annual_Metrics.csv')  
+    Month_met=ReadMetrics('Monthly_Metrics.csv')
+    tippe_met=Ann_met[Ann_met['Station']=='Tippe']
+    wildcat_met=Ann_met[Ann_met['Station']=='Wildcat']
+    
+    
+    #plot annual coeff of var
+    plt.plot(tippe_met["Coeff Var"], 'k*', label='Tippecanoe River')
+    plt.plot(wildcat_met["Coeff Var"], 'k+', label='Wildcat River')
+    plt.xlabel("Year")
+    plt.ylabel("Coefficient of Variation")
+    #ax=plt.gca()
+    #ax.set_xticklabels(np.arange(1969,2019,50))
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+    
+    plt.savefig('coeffvar.png', dpi=96)
+    
+    #plot annual tqmean
+    plt.plot(tippe_met["Tqmean"], 'k*', label='Tippecanoe River')
+    plt.plot(wildcat_met["Tqmean"], 'k+', label='Wildcat River')
+    plt.xlabel("Year")
+    plt.ylabel("TQMean (%)")
+    #ax=plt.gca()
+    #ax.set_xticklabels(np.arange(1969,2019,50))
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig('tqmean.png', dpi=96)
+    
+    #plot annual RBrichards baker flashiness index  
+    
+    
+    plt.plot(tippe_met["R-B Index"], 'k*', label='Tippecanoe River')
+    plt.plot(wildcat_met["R-B Index"], 'k+', label='Wildcat River')
+    plt.xlabel("Year")
+    plt.ylabel("Richards Baker Flashiness Index")
+    #ax=plt.gca()
+    #ax.set_xticklabels(np.arange(1969,2019,50))
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+    plt.savefig('rbindex.png', dpi=96)
+    
+    
+    #calculate exceedence probability
+    
+    ep_tippe=tippe_met.drop(columns=['site_no', 'Mean Flow', 'Median Flow', 'Coeff Var', 'Skew', 'Tqmean', 'R-B Index', '7Q', '3xMedian'])
+    tippe_flow=ep_tippe.sort_values('Peak Flow', ascending=False)
+    tippe_ranks1=stats.rankdata(tippe_flow['Peak Flow'], method='average')
+    tippe_ranks2=tippe_ranks1[::-1]
+    tippe_ep=[(tippe_ranks2[i]/(len(tippe_flow)+1)) for i in range(len(tippe_flow))]
+    
+    
+    ep_wild=wildcat_met.drop(columns=['site_no', 'Mean Flow', 'Median Flow', 'Coeff Var', 'Skew', 'Tqmean', 'R-B Index', '7Q', '3xMedian'])
+    wild_flow=ep_wild.sort_values('Peak Flow', ascending=False)
+    wild_ranks1=stats.rankdata(wild_flow['Peak Flow'], method='average')
+    wild_ranks2=wild_ranks1[::-1]
+    wild_ep=[(wild_ranks2[i]/(len(wild_flow)+1)) for i in range(len(wild_flow))]
+    
+    #plot exceeedence probability
+    plt.plot(tippe_ep, tippe_flow['Peak Flow'], label='Tippecanoe River', color='black')
+    plt.plot(wild_ep, wild_flow['Peak Flow'], label='Wildcat River')
+    plt.xlabel("Exceedence Probability")
+    plt.ylabel("Peak Discharge (CFS)")
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+    
+    
+    plt.savefig('exceedence.png', dpi=96)
+    
+        
+        
+        
+        
     
     
     
-    
-
-
-
 
 
 
